4fa32398f0329d7bcfbe3ec07c94ef8a477e2a41
Ashish S <ashishjsharda@gmail.com>
2022-06-21 10:03:50 -0500

    Create topic_subscriber.py

---
+++ topic_subscriber.py
@@ -0,0 +1,17 @@
+Example 3-2. topic_subscriber.py
+#!/usr/bin/env python
+
+
+import rospy
+from std_msgs.msg import Int32
+
+
+def callback(msg):
+    print msg.data
+
+
+rospy.init_node('topic_subscriber')
+
+sub = rospy.Subscriber('counter', Int32, callback)
+
+rospy.spin()
